85027661bf793af9dd12d9d730559c38f3f9a1ff
AlbertSch8 <161817564+AlbertSch8@users.noreply.github.com>
2025-05-06 20:03:38 +0200

    Update dht_mereni.py

--- dht_mereni.py
+++ dht_mereni.py
@@ -1,4 +1,5 @@
-dht_mereni.py
+
+albert@rasberri:~ $ cat dht_mereni.py 
 
 import time
 import board
@@ -9,9 +10,11 @@ from datetime import datetime
 
 # Sensor data pin is connected to GPIO 4
 sensor = adafruit_dht.DHT22(board.D4)
+# Uncomment for DHT11
 #sensor = adafruit_dht.DHT11(board.D4)
 #now = datetime.datetime.now()
 #con = sqlite3.connect('bertiho_db')
+#cur = con.cursor()
 try:
         # Print the values to the serial port
         temperature_c = sensor.temperature
@@ -20,21 +23,39 @@ try:
         print("{0:%c}  Temp={1:0.1f}ºC, Temp={2:0.1f}ºF, Humidity={3:0.1f}%".format(datetime.now(),temperature_c, temperature_f, humidity))
 
 except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
 except Exception as error:
         sensor.exit()
 
         raise error
 
+#        cur.execute("INSERT INTO  mereni VALUES (temperature_c, humidity, now)")  
 # Connect to a database
 connection = sqlite3.connect('/opt/bertiho_db')
 
 try:
+    # date_time = datetime.now()
     # Database operations
     print("Pokus o zapis do DB bertiho_db a tabulky mereniDHT22")
-    connection.execute("INSERT INTO mereniDHT22 (teplota, vlhkost) VALUES (?, ?)", (temperature_c, humidity))
-    connection.commit()
-    connection.close()
+    cur = connection.cursor()
+    cur.execute("SELECT teplota from  mereniDHT22 order by created_at DESC limit 1")
+    posledni_teplota_DB = cur.fetchall()
+    #print("Nactena posledni hodnota z DB: ")
+    for row in posledni_teplota_DB:
+       print("nactena teplota z DB: ", row[0])
+       teplota_z_DB = row[0]
+    #print(posledni_teplota_DB[0])
+    #print(cur)
+    if teplota_z_DB != temperature_c :
+            print("Pokus o zapis do DB bertiho_db a tabulky mereniDHT22")
+            connection.execute("INSERT INTO mereniDHT22 (teplota, vlhkost) VALUES (?, ?)", (temperature_c, humidity))
+#connection.execute('''INSERT INTO mereniDHT22 (teplota, vlhkost) VALUES (temperature_c, humidity)''' )
+            connection.commit()
+            connection.close()
+    # ...
+    else : 
+            print("Naměřená hodnota je stejná jako poslední - nic nezapisuji.")
     # ...
 except sqlite3.Error as e:
     # Handle the exception
